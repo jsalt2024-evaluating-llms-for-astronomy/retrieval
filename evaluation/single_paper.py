56dcaff681ab41b80d1af203dc1a9ed59ef3ba3f
charlieoneill11 <cponeill00@gmail.com>
2024-06-25 15:30:30 -0400

    cookie time

--- evaluation/single_paper.py
+++ evaluation/single_paper.py
@@ -14,26 +14,6 @@ def random_paper():
     random_paper = os.path.join(path, random_folder, random_paper)
     return random_paper
 
-# random_paper = random_paper()
-random_paper = "/Users/charlesoneill/Desktop/~/Desktop/arxiv-data/0211/astro-ph0211072_arXiv.txt"
-print(random_paper)
-
-# Print the contents of the paper
-# with open(random_paper, 'r') as file:
-#     print(file.read())
-
-# Open the contents of the paper
-contents = open(random_paper, 'r').read()
-introduction = contents.split("Introduction: ")[1].split("Conclusions: ")[0].strip()
-conclusion = contents.split("Conclusions: ")[1].strip()
-# print(f"Introduction: {introduction}\n\n")
-# print('-' * 100)
-# print(f"Conclusions: {conclusion}")
-
-# Assert that both are not None
-assert introduction is not None
-assert conclusion is not None
-
 
 # Create the prompt
 prompt = """
@@ -44,19 +24,53 @@ Here's the introduction: {introduction}
 Based on this, determine one specific astronomy question. Do not refer specifically to the paper - frame it as a general astronomy question. Do not return anything other than the question.
 """
 
-# Fill in the prompt
-prompt = prompt.format(introduction=introduction)
-# print(prompt)
+def claude(prompt, **kwargs):
+    client = anthropic.Anthropic(
+        # defaults to os.environ.get("ANTHROPIC_API_KEY")
+        api_key="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",
+    )
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1024,
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return message.content[0].text
 
-client = anthropic.Anthropic(
-    # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key="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",
-)
-message = client.messages.create(
-    model="claude-3-5-sonnet-20240620",
-    max_tokens=1024,
-    messages=[
-        {"role": "user", "content": prompt}
-    ]
-)
-print(message.content)
+
+# Create the dataset
+NUM_QUESTIONS = 100
+API_KEY = "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",
+question_answer_dict = {}
+
+for i in range(NUM_QUESTIONS):
+
+    introduction = ''
+
+    while len(introduction) < 500:
+        rp = random_paper()
+        contents = open(rp, 'r').read()
+        introduction = contents.split("Introduction: ")[1].split("Conclusions: ")[0].strip()
+
+    # Get random paper folder and file
+    folder_file = rp.split('/')[-2:]
+    # Join them into one string
+    folder_file = '/'.join(folder_file)
+    # print(folder_file)
+    # break
+
+        
+    formatted_prompt = prompt.format(introduction=introduction)
+    question = claude(formatted_prompt)
+
+    question_answer_dict[folder_file] = {'question': question, 'introduction': introduction}
+
+    # Print everything
+    print(f"Question {i+1}: {question}")
+
+    break
+
+
+
+
