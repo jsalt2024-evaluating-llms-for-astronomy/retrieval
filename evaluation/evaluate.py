c7dda5fa1241ef0f232fee1d4ff6cc7f4806154f
charlieoneill11 <cponeill00@gmail.com>
2024-06-27 13:08:39 -0400

    yeo

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -4,6 +4,7 @@ from typing import List, Dict, Any
 from collections import defaultdict
 import wandb
 import numpy as np
+from tqdm import tqdm
 
 class RetrievalSystem(ABC):
     @abstractmethod
@@ -21,17 +22,19 @@ class RetrievalSystem(ABC):
         pass
 
 class Evaluator:
-    def __init__(self, retrieval_system: RetrievalSystem, system_name: str):
+    def __init__(self, retrieval_system: RetrievalSystem, system_name: str, wandb_log: bool = False):
         self.retrieval_system = retrieval_system
         self.system_name = system_name
+        self.wandb_log = wandb_log
 
     def evaluate(self, single_doc_file: str, multi_doc_file: str, top_k: int = 10) -> Dict[str, Dict[str, float]]:
-        wandb.init(project="jsalt-astro", name=f"{self.system_name}")
+        if self.wandb_log:
+            wandb.init(project="jsalt-astro", name=f"{self.system_name}")
 
         results = {}
         
-        single_results = self._evaluate_single_document(single_doc_file, top_k)
-        results['single_doc'] = single_results
+        #single_results = self._evaluate_single_document(single_doc_file, top_k)
+        results['single_doc'] = None #single_results
 
         multi_results = self._evaluate_multi_document(multi_doc_file, top_k)
         results['multi_doc'] = multi_results
@@ -41,8 +44,9 @@ class Evaluator:
             for metric, value in metrics.items():
                 log_dict[f"{eval_type}_{metric}"] = value
         
-        wandb.log(log_dict)
-        wandb.finish()
+        if self.wandb_log:
+            wandb.log(log_dict)
+            wandb.finish()
         
         return results
 
@@ -50,31 +54,49 @@ class Evaluator:
         ground_truth = self._load_ground_truth(ground_truth_file)
         results = defaultdict(list)
         
-        for doc_id, data in ground_truth.items():
-            for question_type in ['intro', 'conclusion']:
-                query = data[f'question_{question_type}']
-                retrieved_docs = self.retrieval_system.retrieve(query, top_k=top_k)
-                
-                results['success_rate'].append(int(doc_id in retrieved_docs))
-                results['reciprocal_rank'].append(self._calculate_reciprocal_rank(retrieved_docs, doc_id))
-                results['avg_precision'].append(self._calculate_avg_precision(retrieved_docs, doc_id))
+        total_queries = len(ground_truth) * 2
+        with tqdm(total=total_queries, desc="Single-doc progress") as pbar:
+            for doc_id, data in ground_truth.items():
+                for question_type in ['intro', 'conclusion']:
+                    query = data[f'question_{question_type}']
+                    retrieved_docs = self.retrieval_system.retrieve(query, top_k=top_k)
+                    
+                    results['success_rate'].append(int(doc_id in retrieved_docs))
+                    results['reciprocal_rank'].append(self._calculate_reciprocal_rank(retrieved_docs, doc_id))
+                    results['avg_precision'].append(self._calculate_avg_precision(retrieved_docs, doc_id))
+
+                    pbar.update(1)
         
         return {metric: sum(values) / len(values) for metric, values in results.items()}
 
-    def _evaluate_multi_document(self, ground_truth_file: str, top_k: int = 10) -> Dict[str, float]:
+    def _evaluate_multi_document(self, ground_truth_file: str, top_k: int = 25) -> Dict[str, float]:
         ground_truth = self._load_ground_truth(ground_truth_file)
         results = defaultdict(list)
         
-        for item in ground_truth:
+        for item in tqdm(ground_truth, desc="Multi-doc progress"):
+            item = item[list(item.keys())[0]]
             query = item['question']
             retrieved_docs = self.retrieval_system.retrieve(query, top_k=top_k)
             relevant_docs = self._map_arxiv_to_filenames(item['arxiv'])
+
+            # Remove 'astro-ph' prefix from arxiv IDs
+            retrieved_docs = [doc.replace('astro-ph', '') for doc in retrieved_docs]
+
+            # Print both the retrieved and relevant documents
+            print(f"\nQuery: {query}")
+            print("Retrieved Documents:")
+            for doc in retrieved_docs:
+                print(f"  {doc}")
+            print("Relevant Documents:")
+            for doc in relevant_docs:
+                print(f"  {doc}")
             
             results['map'].append(self._calculate_map(retrieved_docs, relevant_docs))
             results['ndcg'].append(self._calculate_ndcg(retrieved_docs, relevant_docs))
             results['recall@k'].append(self._calculate_recall_at_k(retrieved_docs, relevant_docs, top_k))
             results['precision@k'].append(self._calculate_precision_at_k(retrieved_docs, relevant_docs, top_k))
             results['f1@k'].append(self._calculate_f1_at_k(retrieved_docs, relevant_docs, top_k))
+            print(results)
         
         return {metric: np.mean(values) for metric, values in results.items()}
 
@@ -85,7 +107,18 @@ class Evaluator:
 
     def _map_arxiv_to_filenames(self, arxiv_ids: List[str]) -> List[str]:
         #self.document_ids.append(f"{paper['subfolder']}/{paper['filename']}")
-        return [f"arxiv/{arxiv_id[:4]}/{arxiv_id}_arXiv.txt" for arxiv_id in arxiv_ids]
+        ids_list = []
+        for arxiv_id in arxiv_ids:
+            if '.' in arxiv_id:
+                print(arxiv_id)
+                year_month = arxiv_id.split('.')[0]
+                identifier = arxiv_id.split('.')[1].split('_')[0]
+                ids_list.append(f"{year_month}/{year_month}.{identifier}_arXiv.txt")
+            else:
+                year_month = arxiv_id[:4]
+                identifier = arxiv_id[4:]
+                ids_list.append(f"{year_month}/{year_month}.{identifier}_arXiv")
+        return ids_list
     
     @staticmethod
     def _calculate_reciprocal_rank(retrieved_docs: List[str], relevant_doc: str) -> float:
--- src/scratch.py
+++ src/scratch.py
@@ -1,16 +1,57 @@
 import json
+from typing import List, Dict
+from datasets import load_dataset
 
 path = "/Users/charlesoneill/retrieval/data/multi_paper.json"
 
 with open(path, 'r') as f:
     data = json.load(f)
 
-# Remove any data where the arxiv field is empty list
-data = [{k: v} for k, v in data.items() if v['arxiv']]
-print(len(data))
+# # Remove any data where the arxiv field is empty list
+# data = [{k: v} for k, v in data.items() if v['arxiv']]
+# print(len(data))
 
-# Save back to multi_paper.json
-with open(path, 'w') as f:
-    json.dump(data, f, indent=4)
+# def _map_arxiv_to_filenames(self, arxiv_ids: List[str]) -> List[str]:
+#         #self.document_ids.append(f"{paper['subfolder']}/{paper['filename']}")
+#         ids_list = []
+#         for arxiv_id in arxiv_ids:
+#             if '.' in arxiv_id:
+#                 print(arxiv_id)
+#                 year_month = arxiv_id.split('.')[0]
+#                 identifier = arxiv_id.split('.')[1].split('_')[0]
+#                 ids_list.append(f"{year_month}/{year_month}.{identifier}_arXiv.txt")
+#             else:
+#                 year_month = arxiv_id[:4]
+#                 identifier = arxiv_id[4:]
+#                 ids_list.append(f"{year_month}/{year_month}.{identifier}_arXiv")
+#         return ids_list
 
-print("Done!")
+# # Apply this to the data
+# for d in data:
+#     key_zero = list(d.keys())[0]   
+#     arxiv_ids = d[key_zero]['arxiv']
+#     d[key_zero]['arxiv'] = _map_arxiv_to_filenames(None, arxiv_ids)
+
+# # Save the updated data
+# with open(path, 'w') as f:
+#     json.dump(data, f)
+    
+
+# # Load the huggingface dataset
+repo_id = "charlieoneill/jsalt-astroph-dataset"
+dataset = load_dataset(repo_id, split="train")
+
+# Get all the filenames from the dataset
+filenames = dataset['filename']
+
+# Only keep citations in the data that are in filenames
+for d in data:
+    key_zero = list(d.keys())[0]
+    arxiv_ids = d[key_zero]['arxiv']
+    d[key_zero]['arxiv'] = [arxiv_id for arxiv_id in arxiv_ids if arxiv_id in filenames]
+
+# Print how many citations for each d in data
+for d in data:
+    key_zero = list(d.keys())[0]
+    print(len(d[key_zero]['arxiv']))
+
